bceacd27c4cd1da4b9e1db0181d581fd87cfbb89
Joe Hattori <joe4.ninjahattori@icloud.com>
2019-12-03 01:05:37 +0900

    download from dropbox
    update

---
+++ dropbox.js
@@ -0,0 +1,28 @@
+const dropboxPath = process.argv[process.argv.length - 1];
+const paths = dropboxPath.split("/");
+const saveFileName = paths[paths.length - 1];
+const dotenv = require("dotenv");
+dotenv.config();
+const token = process.env.DROPBOX_ACCESS_TOKEN;
+if (!token) {
+    console.error("Please set token.");
+    return;
+}
+const fs = require("fs");
+const Dropbox = require("dropbox").Dropbox;
+
+const fetch = require("isomorphic-fetch");
+const dbx = new Dropbox({
+    fetch: fetch,
+    accessToken: token,
+});
+
+const files = dbx.filesDownload({
+    path: dropboxPath,
+}).then(function(result){
+    fs.writeFile(saveFileName, result.fileBinary, "binary", function(error) {
+        if (error) {
+            console.error("Error occurred: ", error);
+        }
+    });
+}, console.error);
